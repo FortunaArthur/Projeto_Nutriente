f50134762a14227ad14fd4e632fefbf71f87a783
FortunaArthur <arthur.trabalho2646@gmail.com>
2026-02-10 00:56:45 -0300

    conect db

--- init.py
+++ init.py
@@ -1,1 +1,8 @@
-print("init repo")
+from connections.database import engine
+
+try:
+    with engine.connect() as connection:
+        print("Conectado ao MySQL com sucesso 🚀")
+except Exception as e:
+    print("Erro ao conectar no banco ❌")
+    print(e)
